3bf7be47180d1728ac8616f014d494b86a622d7f
Uğur Akın <dev.ugur.akin@gmail.com>
2024-05-23 21:54:24 +0300

    pass: Respond with body

--- app/server.go
+++ app/server.go
@@ -9,11 +9,6 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "localhost:4221")
 	fmt.Println(l.Addr())
 	if err != nil {
@@ -29,7 +24,6 @@ func main() {
 			continue
 		}
 
-		// send back 200 OK
 		handleClient(conn)
 	}
 }
@@ -37,26 +31,51 @@ func main() {
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 
-	// parse request and get url target
+	// parse request and get URL target
 	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
+	n, err := conn.Read(buf)
 	if err != nil {
 		log.Println("error reading HTTP request: ", err)
+		return
 	}
 
-	reqRaw := string(buf)
-	path := strings.Split(reqRaw, " ")[1]
+	// extract URL path from request
+	req := string(buf[:n])
+	path := strings.Split(req, " ")[1]
 
+	fmt.Println(path)
+
+	// handle URL target
 	status := "404 Not Found"
-	if path == "/" {
+	body := ""
+	switch {
+	case path == "/":
+		status = "200 OK"
+	case strings.HasPrefix(path, "/echo/"):
+		body = path[6:]
 		status = "200 OK"
 	}
 
+	// write response
 	buf = []byte(fmt.Sprintf("HTTP/1.1 %s\r\n\r\n", status))
-	n, err := conn.Write(buf)
+
+	if body != "" {
+		buf = formatPlain(body)
+	}
+
+	_, err = conn.Write(buf)
 	if err != nil {
 		log.Println("Error writing to connection: ", err)
 		return
 	}
-	log.Printf("wrote %v bytes", n)
+}
+
+// writePlain writes an HTTP response with the plain text s as body.
+func formatPlain(body string) []byte {
+	statusLine := "HTTP/1.1 200 OK\r\n"
+	headers := fmt.Sprintf(
+		"Content-Type: text/plain\r\nContent-Length: %v\r\n\r\n",
+		len([]byte(body)),
+	)
+	return []byte(statusLine + headers + body)
 }
